28c0f9ebfa8d80380ee50c852127cbd10c1f74b7
kbjude <kbjude@gmail.com>
2020-05-10 16:44:24 +0300

    Working show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,12 @@ class ArticlesController < ApplicationController
         redirect_to @article
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     private
     def article_params
        params.require(:article).permit(:title, :text)
-    end
-
-    def show
-        @article = Article.find(params[:id])
     end
 end

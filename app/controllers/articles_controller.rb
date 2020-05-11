977e41811819477ceef63851ceb86532d28565ee
kbjude <kbjude@gmail.com>
2020-05-11 20:41:21 +0300

    edit single item in show and off the list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @articles = Article.new(article_params)
 
@@ -19,6 +23,15 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article
+    else
+      render 'edit'
+    end
   end
 
   private
